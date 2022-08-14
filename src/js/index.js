ce90bfbd214c993db271eec784bb7131eb5571e4
Serov Sergey <serovsergey79@gmail.com>
2022-08-14 10:08:46 +0300

    add: search selection

--- src/js/index.js
+++ src/js/index.js
@@ -14,10 +14,15 @@ const refs = {
   info: document.querySelector(".country-info"),
 }
 
+refs.list.addEventListener('click', (evt) => {
+  refs.input.value = evt.target.closest('li').dataset.name;
+  refs.input.dispatchEvent(new Event('input'));
+})
+
 refs.input.addEventListener('input', debounce(evt => {
   refs.list.textContent = '';
   refs.info.textContent = '';
-  const searchString = evt.target.value.trim();//.toLowerCase();
+  const searchString = evt.target.value.trim().toLowerCase();
   if (!searchString) return;
   fetchCountries(searchString)
     .then((data) => {
@@ -28,11 +33,23 @@ refs.input.addEventListener('input', debounce(evt => {
         } else if (count > 10) {
           Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
         } else {
+          data = data.map(el => {
+            const pos = el.name.official.toLowerCase().indexOf(searchString);
+            if (~pos) {
+              const nameParts = {};
+              nameParts.begin = el.name.official.slice(0, pos);
+              nameParts.searchString = el.name.official.slice(pos, pos + searchString.length);
+              nameParts.end = el.name.official.slice(pos + searchString.length);
+              return { ...el, nameParts };
+            }
+            return el;
+          })
           refs.list.insertAdjacentHTML('beforeend', countryListTpl(data));
         }
       }
     })
-    .catch(() => {
+    .catch((err) => {
+      console.log(err)
       Notiflix.Notify.failure("Oops, there is no country with that name");
     })
 }, DEBOUNCE_DELAY));
