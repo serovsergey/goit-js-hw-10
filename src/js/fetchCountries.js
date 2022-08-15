07ccfa86d61b60fee29b92b95dd411d6688a0c05
Serov Sergey <serovsergey79@gmail.com>
2022-08-15 15:52:18 +0300

    add: searchPath

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,6 @@
 export const fetchCountries = name => {
   return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+    // return fetch(`https://restcountries.com/v3.1/name/${name}`)
     .then(response => {
       if (!response.ok) {
         throw new Error(response.status);
--- src/js/index.js
+++ src/js/index.js
@@ -19,6 +19,35 @@ refs.list.addEventListener('click', (evt) => {
   refs.input.dispatchEvent(new Event('input'));
 })
 
+const getFirstOccurrence = (obj, str, path = '', c = 0) => {
+  if (typeof obj === 'string') {
+    if (obj.toLowerCase().includes(str))
+      return path + '=' + obj;
+  }
+  else if (Array.isArray(obj)) {
+    let res = '';
+    obj.forEach((el, idx) => {
+      const inner = getFirstOccurrence(el, str, path + '[' + idx + ']', c + 1);
+      if (inner) {
+        res = inner;
+        return inner;
+      }
+    })
+    if (res) return res;
+  }
+  else if (typeof obj === 'object' && obj !== null) {
+    for (let f in obj) {
+      // console.log('object', f, obj[f]);
+      const val = obj[f];
+      const inner = getFirstOccurrence(val, str, path + '.' + f, c + 1);
+      if (inner) {
+        return inner;
+      }
+    }
+  }
+  return '';
+}
+
 refs.input.addEventListener('input', debounce(evt => {
   refs.list.textContent = '';
   refs.info.textContent = '';
@@ -33,23 +62,26 @@ refs.input.addEventListener('input', debounce(evt => {
         } else if (count > 10) {
           Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
         } else {
+          // data = data.map(country => ({ ...country, searchPath: getFirstOccurrence(country, searchString).slice(1) }));
           data = data.map(el => {
+            const searchPath = getFirstOccurrence(el, searchString).slice(1);
             const pos = el.name.official.toLowerCase().indexOf(searchString);
             if (~pos) {
               const nameParts = {};
               nameParts.begin = el.name.official.slice(0, pos);
               nameParts.searchString = el.name.official.slice(pos, pos + searchString.length);
               nameParts.end = el.name.official.slice(pos + searchString.length);
-              return { ...el, nameParts };
+              return { ...el, nameParts, searchPath };
             }
-            return el;
+            return { ...el, searchPath };
           })
+          // console.log(data)
           refs.list.insertAdjacentHTML('beforeend', countryListTpl(data));
         }
       }
     })
     .catch((err) => {
-      // console.log(err);
+      console.log(err);
       Notiflix.Notify.failure("Oops, there is no country with that name");
     })
 }, DEBOUNCE_DELAY));
